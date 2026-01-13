bcf0fccc33df898c07947ae2d012b54ad3f84ee7
Benjamin23k <wilsonbenjaminrosa@gmail.com>
2026-01-13 17:56:46 -0400

    Add files via upload

---
+++ HomeWork/Program.cs
@@ -0,0 +1,32 @@
+﻿// Name: Wilson Benjamin rosa Registration: 2025-0968 
+
+
+int number = 19;
+string name = "Benjamin" ;
+
+const double pi = 3.14; 
+// pi = 3.15  <-- sintax Error
+
+Console.WriteLine($"My age is {number}");
+Console.WriteLine($"What is your name? {name}");
+Console.WriteLine(pi);
+
+
+int i = 20;
+
+Console.WriteLine(i);
+i++;
+Console.WriteLine(i);
+++i;
+Console.WriteLine(i);
+
+
+double numbers =10152466.25;
+
+byte b =(byte) (5 + numbers);
+Console.WriteLine(b);
+
+
+DateTime now = DateTime.Now;
+        Console.WriteLine("Current local date/time: " + now);
+
